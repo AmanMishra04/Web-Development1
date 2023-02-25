52f61bcf267f1b265e9cd627b45556d285d1f648
PensPencil <92708776+PensPencil@users.noreply.github.com>
2023-02-25 23:20:11 +0530

    Halcy moved to project.

--- Halcy/script.js
+++
@@ -1,9 +0,0 @@
-const left = document.getElementById("left-side");
-
-const handleOnMove = e =>{
-    const p = e.clientX /window.innerWidth * 100;
-
-    left.style.width = `${p}%`;
-}
-document.onmousemove = e => handleOnMove(e);
-document.ontouchmove = e =>handleOnMove(e.touches[0]);
